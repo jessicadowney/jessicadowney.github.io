5f584b45a4719ee328a8f98632efcd5b2970930f
Jessica Downey <hello@jessicadowney.com>
2020-09-26 20:17:49 +0200

    Update to portfolio 2020

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
 $(document).ready(function() {
 
 
-var jdfaces = [{{ site.url }}/"/images/jdwave.gif", {{ site.url }}/"/images/jdwink.gif"];
-
-
-$( ".gifportrait" ).hover(function() {
-
-	$(".gifportrait").attr("src", jdfaces[Math.floor(Math.random()*jdfaces.length)])
-
-
-})
 
 
 
